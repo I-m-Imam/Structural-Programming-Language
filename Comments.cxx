0fd1a534c6f1045477695fbba44162d73c16577f
I-m-Imam <mdimamakib24@gmail.com>
2026-09-08 12:46:37 +0600

    Add files via upload

---
+++ Comments.cxx
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  // This is a single line comment.
+  printf("Hello World!\n");
+  /* This is a multiline comment.
+  printf("I am Imam.\n");*/
+  printf("I am learning C.");
+  return 0;
+}
